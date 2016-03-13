ed54964155d347693fffd46432db0c357a9aa8d5
Rafael <rafael_baur@web.de>
2016-03-13 23:31:38 +0100

    neue Dateien

--- TuS-Festzelt-Reservierung/formular.php
+++ TuS-Festzelt-Reservierung/formular.php
@@ -10,9 +10,10 @@
 	
 	    return $key;
 	}
-
-	$table = $_POST['btn'];
-	$uniqID= random_string(10);
+	
+	//var_dump($_POST);
+	$tbl = $_POST['tischliste'];
+	$uniqID = random_string(10);
 
 ?>
 
@@ -23,70 +24,23 @@
 <meta content="de" http-equiv="Content-Language" />
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <title>Untitled 1</title>
-<style type="text/css">
-.auto-style1 {
-	margin-top: 0px;
-}
-.auto-style2 {
-	margin-top: 19px;
-}
-</style>
 </head>
 
-<body background="Bierzelt.jpg" style="font-size: 1cm" >
+<body>
 
-<label id="Label1" style="font-family: Cambria, Cochin, Georgia, Times, &quot;Times New Roman&quot;, serif; font-size: xx-large">
-Buchungsformular für Tisch: <?php echo $table; ?></label>
-<label id="Label2" style="font-family: Cambria, Cochin, Georgia, Times, &quot;Times New Roman&quot;, serif; font-size: xx-large">
-<br>uniqueID: <?php echo $uniqID; ?></label>
-<br>
 <form action="skript.php" method="post">
-<input id="lbl1" type="hidden" name="table" value="<?php print $table; ?> "style='width: 0cm; height: 0cm'></input>
-<table border="0" cellspacing="0" cellpadding="2" class="auto-style2">
-  <tbody style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size: 1cm">
-    <tr>
-      <td>Anrede:</td>
-        <td>
-        <input checked="checked" name="Anrede" type="radio" value="Herr" /> Herr
-        <input name="Anrede" type="radio" value="Frau" /> Frau
-      </td>
-    </tr>
-    <tr>
-      <td>Vorname:</td>
-      <td style="height: 29px">
-        <input maxlength="50" name="Vorname" size="45" type="text" class="auto-style1" style="height: 31px" />
-      </td>
-    </tr>
-    <tr>
-      <td>Nachname:</td>
-      <td>
-        <input name="Nachname" size="45" type="text" style="height: 33px" />
-      </td>
-    </tr>
-    <tr>
-      <td>Email:</td>
-      <td>
-        <input name="Email" size="45" type="text" style="height: 30px" />
-      </td>
-    </tr>
-    <tr>
-      <td style="height: 142px">Mitteilung:</td>
-      <td style="height: 142px">
-	  <textarea name="Mitteilung" style="width: 273px; height: 128px">Irgendwelche Fragen???</textarea></td>
-    </tr>
-    <tr>
-      	<td></td>
-      	<td><button 'type='submit' name='formBtn' value='OK' style='width: 4cm; height: 2cm; font-size: 7mm;' />Ok</td>
-    </tr>
-    <tr>
-		<td></td>
-		<td><button 'type='submit' name='formBtn' value='Cancel' style='width: 4cm; height: 2cm; font-size: 7mm;' />Cancel</td>
-    </tr>
-  </tbody>
-</table>
+	<input id="lbl1" type="text" name="tables" value="<?php "Ausgewählte Tische: " . print $tbl ; ?>" style="width: 483px"></input>
+	<br><label id="Label1" style="height: 1.5em; width: 76px;">Vorname: </label> <input name="Text1" type="text" />
+	<br><label id="Label2" style="height: 1.6em; width: 76px;">Nachname: </label> 
+	<input name="Text2" type="text" /> <br>
+	<label id="Label3" style="height: 1.5em; width: 76px;">Straße </label> 
+	<input name="Text3" type="text" /> <br>
+	<label id="Label4" style="height: 1.5em; width: 76px;">Stadt: </label> 
+	<input name="Text4" type="text" /> <br>
+	<label id="Label5" style="height: 1.5em; width: 76px;">PLZ: </label> 
+	<input name="Text5" type="text" />
 </form>
 
 </body>
 
 </html>
-
