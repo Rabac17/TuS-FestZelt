c3021d36ffa7167747a9fef4a1f7e49cd8ab98e4
Rafael <rafael_baur@web.de>
2016-03-13 12:12:17 +0100

    new Files

---
+++ TuS-Festzelt-Reservierung/zeltplanV3.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Untitled 1</title>
+<style type="text/css">
+input[type=button] {
+    width: 4.5em;  height: 1.5em;
+    
+}
+.auto-style1 {
+	margin-left: 0px;
+}
+.auto-style2 {
+	margin-left: 1px;
+}
+</style>
+</head>
+
+<body>
+
+<form method="post" style="background-color: #808000; font-size: xx-small; width: 1009px;" class="auto-style2">
+	<br />
+	&nbsp;&nbsp;&nbsp;
+	<input name="N1" type="button" value="DRK" style="background-color: #808080; color: #000000;" onclick="myFunction(this)" />
+	<input name="M1" type="button" value="DRK" style="background-color: #808080; color: #000000;" onclick="myFunction(this)"/>&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L1" type="button" value="L1" onclick="myFunction(this)"/>
+	<input name="K1" type="button" value="K1" onclick="myFunction(this)"/>&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J1" type="button" value="J1" onclick="myFunction(this)"/>
+	<input name="I1" type="button" value="I1" onclick="myFunction(this)"/>&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H1" type="button" value="H1" onclick="myFunction(this)"/>
+	<input name="G1" type="button" value="G1" onclick="myFunction(this)"/>&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F1" type="button" value="TuS" onclick="myFunction(this)" style="background-color: #00FFFF" disabled="true" />
+	<input name="E1" type="button" value="TuS" onclick="myFunction(this)" style="background-color: #00FFFF" disabled="true" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D1" type="button" value="D1" onclick="myFunction(this)"/> <br />
+
+	&nbsp;&nbsp;&nbsp;
+
+	<input name="N2" type="button" value="DRK" onclick="myFunction(this)" style="background-color: #808080; color: #000000;" visible="false" />
+	<input name="M2" type="button" value="DRK" onclick="myFunction(this)" style="background-color: #808080; color: #000000;" visible="false" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L2" type="button" value="L2" onclick="myFunction(this)" />
+	<input name="K2" type="button" value="K2" onclick="myFunction(this)" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J2" type="button" value="J2" onclick="myFunction(this)" />
+	<input name="I2" type="button" value="I2" onclick="myFunction(this)" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H2" type="button" value="H2" onclick="myFunction(this)" />
+	<input name="G2" type="button" value="G2" onclick="myFunction(this)" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F2" type="button" value="TuS" onclick="myFunction(this)" style="background-color: #00FFFF" disabled="true" />
+	<input name="E2" type="button" value="TuS" onclick="myFunction(this)" style="background-color: #00FFFF" disabled="true" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D2" type="button" value="D2" onclick="myFunction(this)" />
+	<input name="C2" type="button" value="C2" onclick="myFunction(this)" /> <br />
+	
+	&nbsp;&nbsp;&nbsp;
+	
+	<input name="N3" type="button" value="N3" />
+	<input name="M3" type="button" value="M3" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L3" type="button" value="L3" />
+	<input name="K3" type="button" value="K3" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J3" type="button" value="J3" />
+	<input name="I3" type="button" value="I3" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H3" type="button" value="H3" />
+	<input name="G3" type="button" value="G3" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F3" type="button" value="F3" />
+	<input name="E3" type="button" value="E3" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D3" type="button" value="D3" />
+	<input name="C3" type="button" value="C3" /> <br />
+
+	&nbsp;&nbsp;&nbsp;
+
+	<input name="N4" type="button" value="N4" />
+	<input name="M4" type="button" value="M4" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L4" type="button" value="L4" />
+	<input name="K4" type="button" value="K4" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J4" type="button" value="J4" />
+	<input name="I4" type="button" value="I4" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H4" type="button" value="H4" />
+	<input name="G4" type="button" value="G4" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F4" type="button" value="F4" />
+	<input name="E4" type="button" value="E4" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D4" type="button" value="D4" />
+	<input name="C4" type="button" value="C4" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="B4" type="button" value="B4" />
+	<input name="A4" type="button" value="A4" /><br />
+	
+	&nbsp;&nbsp;&nbsp;
+	
+	<input name="N5" type="button" value="N5" />
+	<input name="M5" type="button" value="M5" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L5" type="button" value="L5" />
+	<input name="K5" type="button" value="K5" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J5" type="button" value="J5" />
+	<input name="I5" type="button" value="I5" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H5" type="button" value="H5" />
+	<input name="G5" type="button" value="G5" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F5" type="button" value="F5" />
+	<input name="E5" type="button" value="E5" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D5" type="button" value="D5" />
+	<input name="C5" type="button" value="C5" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="B5" type="button" value="B5" />
+	<input name="A5" type="button" value="A5" /> <br />
+
+	&nbsp;&nbsp;&nbsp;
+
+	<input name="N6" type="button" value="N6" />
+	<input name="M6" type="button" value="M6" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L6" type="button" value="L6" />
+	<input name="K6" type="button" value="K6" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J6" type="button" value="J6" />
+	<input name="I6" type="button" value="I6" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H6" type="button" value="H6" />
+	<input name="G6" type="button" value="G6" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F6" type="button" value="F6" />
+	<input name="E6" type="button" value="E6" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D6" type="button" value="D6" />
+	<input name="C6" type="button" value="C6" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="B6" type="button" value="B6" />
+	<input name="A6" type="button" value="A6" /> <br />
+
+	&nbsp;&nbsp;&nbsp;
+
+	<input name="N7" type="button" value="N7" />
+	<input name="M7" type="button" value="M7" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L7" type="button" value="L7" />
+	<input name="K7" type="button" value="K7" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J7" type="button" value="J7" />
+	<input name="I7" type="button" value="I7" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H7" type="button" value="Technik" style="background-color: #808080; color: #000000;" disabled="true" />
+	<input name="G7" type="button" value="Technik" style="background-color: #808080; color: #000000;" disabled="true" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F7" type="button" value="F7" />
+	<input name="E7" type="button" value="E7" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D7" type="button" value="D7" />
+	<input name="C7" type="button" value="C7" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="B7" type="button" value="B7" />
+	<input name="A7" type="button" value="A7" /> <br />
+
+	&nbsp;&nbsp;&nbsp;
+
+	<input name="N8" type="button" value="N8" />
+	<input name="M8" type="button" value="M8" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L8" type="button" value="L8" />
+	<input name="K8" type="button" value="K8" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J8" type="button" value="J8" />
+	<input name="I8" type="button" value="I8" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H8" type="button" value="Technik" style="background-color: #808080; color: #000000;" disabled="true" />
+	<input name="G8" type="button" value="Technik" style="background-color: #808080; color: #000000;" disabled="true" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F8" type="button" value="F8" />
+	<input name="E8" type="button" value="E8" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D8" type="button" value="D8" />
+	<input name="C8" type="button" value="C8" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="B8" type="button" value="B8" class="auto-style1" />
+	<input name="A8" type="button" value="A8" /> <br />
+
+	&nbsp;&nbsp;&nbsp;
+
+	<input name="N9" type="button" value="N9" />
+	<input name="M9" type="button" value="M9" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L9" type="button" value="L9" />
+	<input name="K9" type="button" value="K9" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J9" type="button" value="J9" />
+	<input name="I9" type="button" value="I9" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H9" type="button" value="H9" />
+	<input name="G9" type="button" value="G9" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F9" type="button" value="F9" />
+	<input name="E9" type="button" value="E9" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D9" type="button" value="D9" />
+	<input name="C9" type="button" value="C9" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="B9" type="button" value="B9" />
+	<input name="A9" type="button" value="A9" /> <br />
+
+	&nbsp;&nbsp;&nbsp;
+
+	<input name="N10" type="button" value="N10" />
+	<input name="M10" type="button" value="M10" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L10" type="button" value="L10" />
+	<input name="K10" type="button" value="K10" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J10" type="button" value="J10" />
+	<input name="I10" type="button" value="I10" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H10" type="button" value="H10" />
+	<input name="G10" type="button" value="G10" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F10" type="button" value="F10" />
+	<input name="E10" type="button" value="E10" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="D10" type="button" value="D10" />
+	<input name="C10" type="button" value="C10" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="B10" type="button" value="B10" />
+	<input name="A10" type="button" value="A10" /> <br />
+
+	&nbsp;&nbsp;&nbsp;
+
+	<input name="N11" type="button" value="N11" />
+	<input name="M11" type="button" value="M11" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="L11" type="button" value="L11" />
+	<input name="K11" type="button" value="K11" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="J11" type="button" value="J11" />
+	<input name="I11" type="button" value="I11" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="H11" type="button" value="H11" />
+	<input name="G11" type="button" value="G11" />&nbsp;&nbsp;&nbsp;&nbsp;
+	<input name="F11" type="button" value="F11" />
+	<input name="E11" type="button" value="E11" /> <br />
+&nbsp;</form>
+
+<p>&nbsp;</p>
+
+
+<p>
+<strong style="box-sizing: initial; color: rgb(62, 61, 64); font-family: Arial, sans-serif; font-size: 10.6667px; font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: center; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255);">
+Hinweis:</strong><span style="color: rgb(62, 61, 64); font-family: Arial, sans-serif; font-size: 10.6667px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: center; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: rgb(255, 255, 255);"><span class="Apple-converted-space">&nbsp;</span>Die 
+gezeigte Abbildung des Bestuhlungsplans hat nur Symbolcharakter. Es ist nicht 
+gewährleistet dass die Anzahl und Anordnung der Tische der endgültigen 
+Bestuhlung im Festzelt entsprechen.</span></p>
+
+<form id="bookingForm" action='formular.php' method="post">
+	<input id='tischliste' type="text" name='tischliste' value=""/>
+	<input name="btnBuchen" type="button" value="Buchen" style="width: 6em; height: 2.2em;" onclick="submTL()"/></form>
+
+	<script>
+		function submTL(){
+			if(document.getElementsByName('tischliste')[0].value != "") {
+		    	document.forms['bookingForm'].submit();
+		    }
+		}
+	</script>
+&nbsp;</form>
+
+
+</body>
+
+<script>
+
+	document.getElementsByName("N1")[0].disabled = true;
+	document.getElementsByName("M1")[0].disabled = true;
+	document.getElementsByName("N2")[0].disabled = true;
+	document.getElementsByName("M2")[0].disabled = true;
+	
+	//document.getElementsByName("M1")[0].style.backgroundColor = 'grey';
+	
+	function enableButton(name, enable) {
+		if(enable == 'true') {
+			document.getElementsByName(name)[0].disabled = false;
+		
+		}		
+		else {
+			document.getElementsByName(name)[0].disabled = true;
+		
+		}
+	
+	
+	}
+	
+	function myFunction(elmnt) {
+	 	if(elmnt.style.backgroundColor == 'blue') {
+	    	elmnt.style.backgroundColor = 'grey';
+	    	var delTbl = elmnt.value;
+	    	delTbl = delTbl.concat(",");
+	    	var strDel = document.getElementById('tischliste').innerHTML;
+	    	var resDel = strDel.replace(delTbl,"");
+	
+			//document.getElementById('tischliste').innerHTML = resDel; //concat(elmnt.value);		
+			document.getElementById('tischliste').value = resDel; //concat(elmnt.value);
+		    document.getElementById('tischliste').innerHTML = document.getElementById('tischliste').value; //concat(elmnt.value); 
+	
+	    }
+		else {
+	    	elmnt.style.backgroundColor = 'blue';
+	//    	document.getElementById("tischliste").InnerHTML = elmnt.value;
+			var resAdd = document.getElementById('tischliste').innerHTML;
+			resAdd = resAdd .concat(elmnt.value);
+			resAdd = resAdd .concat(",");
+			document.getElementById('tischliste').value = resAdd ; //concat(elmnt.value);	
+	    	document.getElementById('tischliste').innerHTML = document.getElementById('tischliste').value; //concat(elmnt.value); 
+			
+	    }
+	
+	}
+</script>
+
+
+</html>
